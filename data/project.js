e43f5b50c1e93fa56e063a33063cc56adb37919d
spradhuman <shrestha@veriteworks.co.jp>
2022-01-31 23:10:20 +0900

    image changed

--- data/project.js
+++
@@ -1,7 +0,0 @@
-export const project = [
-    {
-        title : "React Portfolio Template",
-        language: "HTML, Scss, JavaScript, React, React-Icons",
-        purpose: "For Personel Portfolio Website Use",
-    }
-]
